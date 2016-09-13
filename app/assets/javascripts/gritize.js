cf4bd869cdc2471f9bdc6620b822646a3edaffc7
corya0687 <corya0687@gmail.com>
2016-09-12 21:22:59 -0400

    updated gritize.js file order or provider argments in config function, updated ur router injection

--- app/assets/javascripts/gritize.js
+++ app/assets/javascripts/gritize.js
@@ -1,10 +1,10 @@
 angular
-  .module('gritize',['ui-router', 'templates'])
-  .config(['$stateProvider', '$urlRouterProvider', function ($urlRouterProvider, $stateProvider) {
+  .module('gritize',['ui.router', 'templates'])
+  .config(['$stateProvider', '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '/home/_home.html',
+        templateUrl: 'home/_home.html',
         controller: 'HomeCtrl'
       });
       $urlRouterProvider.otherwise('home');
