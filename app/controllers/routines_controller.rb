f188e9b57a02b8e2f69576fd8b85ff1a0795a861
corya0687 <corya0687@gmail.com>
2016-09-14 06:05:29 -0400

    build index and private routine controller methods rails backend

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,5 +1,21 @@
 class RoutinesController < ApplicationController
+  before_action :set_routine only: [:show]
+
   def index
-    @
+    @routines = Routine.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_routine
+    @routine = Routine.find(params[:id])
+  end
+
+  def routine_params
+    params.require(:routine).permit(:name, :description, :end_date :tags => [])
   end
 end
