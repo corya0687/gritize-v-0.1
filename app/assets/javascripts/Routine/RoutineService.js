a286281d968d953abfcfdf149a260be68dfb4872
corya0687 <corya0687@gmail.com>
2016-09-14 19:21:33 -0400

    added routines api index call to routineService

--- app/assets/javascripts/Routine/RoutineService.js
+++ app/assets/javascripts/Routine/RoutineService.js
@@ -1,4 +1,8 @@
 function RoutineService($http) {
+  this.getRoutineIndex = function(){
+    return $http.get('/routines.json')
+  }
+
   this.getRoutine = function(id) {
     return $http.get('/routines/' + id + '.json')
   }
