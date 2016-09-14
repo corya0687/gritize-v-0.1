df43e0dc839630e11fc98a6e5b69a0f62969d211
corya0687 <corya0687@gmail.com>
2016-09-13 22:04:54 -0400

    added tags resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160914014921) do
+ActiveRecord::Schema.define(version: 20160914020418) do
 
   create_table "routines", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
