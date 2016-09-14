c1acff4474e4b1f1a365c450f4e7d47e4c8f3aab
corya0687 <corya0687@gmail.com>
2016-09-13 22:08:52 -0400

    removed join table controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :routine_tags
   resources :tags
   resources :routines
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
