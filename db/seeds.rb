cae77431713bd8d29610acb5d313e023fede226e
corya0687 <corya0687@gmail.com>
2016-09-14 05:35:41 -0400

    created seed data for routines and tags

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ro = Routine.Create(name:"Afternoon Nap", description:"Nap on lunchbreak")
+ro = Routine.Create(name:"Afternoon Nap", description:"Nap on lunchbreak", end_date: '2016-08-10')
+ro.tags.build(name:"sleep")
+ro.tags.build(name:"re-energize")
+
+run = Routine.Create(name:"Daily Run", description:"Rune every day at 6", end_date: '2016-09-23')
+run.tags.build(name:"fitness")
+
+ro = Routine.Create(name:"Read", description:"Read something new", end_date: '2016-12-25')
+ro.tags.build(name:"Education")
+ro.tags.build(name:"knowledge")
