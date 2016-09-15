f4bddc51f6c64993aeaba075c066b150c2c2ecf9
corya0687 <corya0687@gmail.com>
2016-09-14 21:38:28 -0400

    added redirects to devise login/registraions states

--- app/assets/javascripts/gritize.js
+++ app/assets/javascripts/gritize.js
@@ -22,13 +22,23 @@ angular
       .state('login', {
         url: '/login',
         templateUrl: 'auth/_login.html',
-        controller: 'AuthController'
+        controller: 'AuthController',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
       })
-      
+
       .state('register', {
         url: '/register',
         templateUrl: 'auth/_register.html',
-        controller: 'AuthController'
+        controller: 'AuthController',
+        onEnter: ['$state', 'Auth', function($state, Auth) {
+          Auth.currentUser().then(function (){
+            $state.go('home');
+          })
+        }]
       })
 
       .state('routine', {
