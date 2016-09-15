3a7ea06baee510661f8dc5249debec43bdf00b00
corya0687 <corya0687@gmail.com>
2016-09-15 17:03:24 -0400

    added routines index route back only to test newRoComponent 2 way bingins

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   resources :tags
   resources :users do
     resources :routines
-
   end
+
+  resources :routines, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#home'
