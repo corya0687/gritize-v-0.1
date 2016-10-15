19ecd92d74a86f709de28bebf21f1efaafa5f2db
corya0687 <corya0687@gmail.com>
2016-10-15 01:44:53 -0400

    added debounce update method
    git push

--- app/assets/javascripts/routine/directives/RoutineTableRow.js
+++ app/assets/javascripts/routine/directives/RoutineTableRow.js
@@ -2,11 +2,28 @@
   function RoutineTableRow() {
     return {
       restrict: 'EA',
-      controller: function () {
+      controller: function ($scope, $timeout, RoutinesService) {
         var vm = this
-        debugger;
+        var timeout;
+
         vm.routine.end_date = new Date(vm.routine.end_date)
         vm.routine.start_date = new Date(vm.routine.start_date)
+        vm.saveRoutine = saveRoutine;
+
+        function saveRoutine() {
+          RoutinesService.saveRoutine(routine)
+        }
+
+        var debouceSaveUpdates = function (newValue, OldValue) {
+          if (newValue != oldVal){
+            if(timeout){
+              $timeout.cancel(timeout)
+            }
+
+            timeout = $timeout(saveUpdates, 1000);
+          }
+        };
+        $scope.$watch('vm.routine', debounceSaveUpdates);
       },
       controllerAs: 'routineCtrl',
       templateUrl: 'routine/views/_routine.html',
@@ -17,9 +34,6 @@
       bindToController: true
     }
   }
-
-
-
 
   angular
     .module('gritize')
