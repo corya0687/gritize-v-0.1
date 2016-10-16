15848556015b926b5ee8335061476e78e84398c9
corya0687 <corya0687@gmail.com>
2016-10-16 09:59:08 -0400

    fixed toggle for edit in routine

--- app/assets/javascripts/routine/directives/RoutineTableRow.js
+++ app/assets/javascripts/routine/directives/RoutineTableRow.js
@@ -4,21 +4,21 @@
       restrict: 'EA',
       controller: function ($scope,RoutinesService) {
         var vm = this;
-        var active_edit = '';
+        var active_edit = false
         vm.routine.end_date = new Date(vm.routine.end_date);
         vm.routine.start_date = new Date(vm.routine.start_date);
         vm.active_edit = active_edit;
         vm.toggle_edit = toggle_edit
 
+
         function toggle_edit() {
-          debugger;
-          if ( active_edit === false || active_edit === '') {
+          if ( vm.active_edit === false || vm.active_edit === '') {
             vm.active_edit = true;
           } else {
             vm.active_edit = false;
           }
         }
-        
+
         $scope.$watch('routineCtrl.routine',
         function saveUpdates(newVal, oldVal) {
           if ( newVal !== oldVal) {
