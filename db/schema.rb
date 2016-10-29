b723b292d0ba2b53749d9849c36a81b9a436944a
corya0687 <corya0687@gmail.com>
2016-10-29 19:39:21 -0400

    migrated database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20161014232926) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "routine_tags", force: :cascade do |t|
     t.integer  "routine_id"
     t.integer  "tag_id"
@@ -26,7 +29,6 @@ ActiveRecord::Schema.define(version: 20161014232926) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.date     "end_date"
-    t.date     "start_date"
   end
 
   create_table "subroutines", force: :cascade do |t|
@@ -60,8 +62,8 @@ ActiveRecord::Schema.define(version: 20161014232926) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
