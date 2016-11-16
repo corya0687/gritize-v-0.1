6f100e58f0c12365ee2a0bcd7b94da60d36d6295
corya0687 <corya0687@gmail.com>
2016-11-16 10:03:34 -0500

    seperated migrations to two seperate

--- db/migrate/20161014232926_add_start_date_to_routines.rb
+++ db/migrate/20161014232926_add_start_date_to_routines.rb
@@ -1,11 +1,9 @@
 class AddStartDateToRoutines < ActiveRecord::Migration[5.0]
   def up
-    change_column :routines, :end_date, :date
     add_column :routines, :start_date, :date
   end
 
   def down
-    change_column :routines, :end_date, :datetime
     remove_column :routines, :start_date, :date
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014232926) do
+ActiveRecord::Schema.define(version: 20161116150153) do
 
   create_table "routine_tags", force: :cascade do |t|
     t.integer  "routine_id"
